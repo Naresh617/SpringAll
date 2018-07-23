83bbd49c3920307fdc324324f8db2d68a233a654
Naresh Reddy <nareshreddynagiri@gmail.com>
2018-07-23 21:33:44 +0530

    Producing Webservices using soap

---
+++ ProducingSoapWebServices/src/main/java/com/naresh/ProducingSoapWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.naresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducingSoapWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducingSoapWebServicesApplication.class, args);
+	}
+}
